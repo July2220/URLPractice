1b77d6447a39bed4bd86b1a6f1c82eddd0bf2428
July Tao <daymatters@protonmail.com>
2023-03-22 16:58:54 +0800

    Initial Commit

---
+++ URLPractice/URLPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLPracticeApp.swift
+//  URLPractice
+//
+//  Created by july on 2023/3/22.
+//
+
+import SwiftUI
+
+@main
+struct URLPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
